e0f299fac9805175cb6e22250be297e6584f2edb
amosayoraombuna <amosayora@gmail.com>
2024-10-24 15:36:54 +0300

    Add files via upload

---
+++ 2D.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int i,j;
+    int marks[2][3]={{35,45,25},{30,45,26}};
+    for(i=0;i<2;i++){
+        for(j=0;j<3;j++){
+        printf("marks[%d][%d]=%d \n",i,j,marks[i][j]);
+        }
+        
+        
+        }
+    return 0;
+    
+    }
+       
+       
+    
+       
+
+
+
+    
