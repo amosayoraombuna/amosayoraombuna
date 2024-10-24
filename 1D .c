0d66c1ced2a8299122b21759cec499821a818925
amosayoraombuna <amosayora@gmail.com>
2024-10-24 15:36:18 +0300

    Add files via upload

---
+++ 1D .c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int i;
+    int marks[6]={23,40,34,50,21,26};
+    for(i=0;i<7;i++){
+        printf("marks[%d] = %d\n",i,marks[i]);
+        }
+        
+    
+    
+    return 0;
+    
+    }
+       
+       
+    
+       
+
+
+
+    
